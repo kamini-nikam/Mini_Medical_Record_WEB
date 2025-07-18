1ce1d808b2b31bbfc8adf9f08aa2bed6204de62b
kamini-nikam <mininikam77@gmail.com>
2025-07-18 16:33:54 +0530

    Add files via upload

---
+++ Mini_Medical_Record_WEB/Mini_Medical_Record_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mini_Medical_Record_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
